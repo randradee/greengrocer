df701eaada3fea39d8d15f1dfaddce94b50c565c
randradee <dev.renatopandrade@gmail.com>
2024-03-21 23:33:05 -0300

    leave only necessary flutter files

--- android/app/src/main/kotlin/com/example/greengrocer/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.greengrocer
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:greengrocer/src/auth/components/custom_text_field.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:greengrocer/src/auth/sign_up_screen.dart';
