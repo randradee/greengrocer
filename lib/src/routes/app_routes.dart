b2c66ec4dbd1135dc523a7c6ba46d0be6730557e
randradee <dev.renatopandrade@gmail.com>
2024-04-29 23:39:38 -0300

    feat: binding da BaseScreen com HomeController

--- lib/src/routes/app_routes.dart
+++ lib/src/routes/app_routes.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:greengrocer/src/bindings/home_binding.dart';
 import 'package:greengrocer/src/pages/auth/sign_in_screen.dart';
 import 'package:greengrocer/src/pages/auth/sign_up_screen.dart';
 import 'package:greengrocer/src/pages/base/base_screen.dart';
@@ -6,10 +7,25 @@ import 'package:greengrocer/src/pages/splash/splash_screen.dart';
 
 abstract class AppRoutes {
   static final pages = <GetPage>[
-    GetPage(name: '/splash', page: () => const SplashScreen()),
-    GetPage(name: '/signin', page: () => SignInScreen()),
-    GetPage(name: '/signup', page: () => SignUpScreen()),
-    GetPage(name: '/', page: () => const BaseScreen()),
+    GetPage(
+      name: '/splash',
+      page: () => const SplashScreen(),
+    ),
+    GetPage(
+      name: '/signin',
+      page: () => SignInScreen(),
+    ),
+    GetPage(
+      name: '/signup',
+      page: () => SignUpScreen(),
+    ),
+    GetPage(
+      name: '/',
+      page: () => const BaseScreen(),
+      bindings: [
+        HomeBinding(),
+      ],
+    ),
   ];
 }
 
